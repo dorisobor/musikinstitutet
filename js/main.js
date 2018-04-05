94934c5e51954704e592e4e82d1992c9bef50f0e
Dragoness390 <cera11@hotmail.se>
2018-04-05 16:29:14 +0200

    updated display tracks, printed the tracks, updated structure

--- js/main.js
+++ js/main.js
@@ -1,34 +1,31 @@
- // get all 5 tracks
-let globalTrackList = []; 
+const content = document.getElementById("content");
 
- // Load all tracks
- fetchTracks();
+// get all 5 tracks
+let globalTrackList = [];
+
+// Load all tracks
+fetchTracks();
 
 // Fetch function
-function fetchTracks () {
-    
-    fetch (`https://folksa.ga/api/tracks?limit=5&key=flat_eric` )
-    .then ((response) => response.json())
-    .then ((trackData) => { 
-    displayTracks(trackData)
-        
-    })
-    .catch((error) => { 
-        console.log(error);
-    })
+function fetchTracks() {
+
+    fetch(`https://folksa.ga/api/tracks?limit=5&key=flat_eric`)
+        .then((response) => response.json())
+        .then(function (tracks) {
+            globalTrackList = tracks;
+
+            // loop through all tracks 
+            let htmlBlock = "";
+            for (const track of tracks) {
+                htmlBlock += `
+<p>${track.title} by ${track.artists.name} </p>
+`;
+            }
+            content.innerHTML = htmlBlock;
+        })
+
+
+        .catch((error) => {
+            console.log(error)
+        });
 }
-
-// Function to display the tracks
-function displayTracks (trackData) {
-    const content = document.getElementById("content");
-    const results = 
-    
-} 
-    
-
-    
-    
-    
-
-    
-
